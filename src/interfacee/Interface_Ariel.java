fa8abad12ed62364791f09a7056fc8ee566a9b7f
aryeeeeeeeeel <arielsumantin69@gmail.com>
2023-11-23 19:14:50 +0800

    Removed @override

--- src/interfacee/Interface_Ariel.java
+++ src/interfacee/Interface_Ariel.java
@@ -2,11 +2,9 @@ package interfacee;
 
 class Interface_Ariel implements Interface_Student{
     
-          @Override
           public void studentCute(){
               System.out.println("Si Ariel kay grabe ka Cutie <3");
           }
-          @Override
           public void coding(){
               System.out.println("Galabad na ang ulo ni Ariel sigeg code :|");
           }    
