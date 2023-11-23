11dda6024f554fb1853cafdd0b6c2b224c4d3636
aryeeeeeeeeel <arielsumantin69@gmail.com>
2023-11-23 19:16:14 +0800

    Removed @override

--- src/interfacee/Interface_Zyrel.java
+++ src/interfacee/Interface_Zyrel.java
@@ -2,11 +2,9 @@ package interfacee;
 
 class Interface_Zyrel implements Interface_Student{
     
-          @Override
           public void studentCute(){
               System.out.println("Si Zyrel ang aking CutiePieBabyBoy <3");
           }
-          @Override
           public void coding(){
               System.out.println("Galabad na ang ulo ni Zyrel sigeg code :|");
           }
